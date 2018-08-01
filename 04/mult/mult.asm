5ddc6aea763c9878f6ba2f66f836264a82e0ac16
索明明 <969929899@qq.com>
2018-08-01 12:11:39 +0800

    Update mult.asm

--- 04/mult/mult.asm
+++ 04/mult/mult.asm
@@ -7,3 +7,29 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+//  i=0
+//  while i < R1
+//  R2 += R0
+//  i++
+@i
+M=0     //i=0
+@R2
+M=0
+(START)
+@i
+D=M
+M=M+1  // i++
+@R1
+D=M  
+@R2
+M=D+M  //R2+=R1
+
+@i
+D=M
+@R1
+D=D-M // if i-R1<0 goto START
+@START
+D;JLT
+(END)
+@END
+0;JEQ
